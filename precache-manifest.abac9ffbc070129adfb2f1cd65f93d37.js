4e48ef63c6cf1bf4b5b2fdae9384236764c36b90
juan3492 <juanpunky3492@gmail.com>
2020-01-15 16:52:59 -0300

    Updates

---
+++ precache-manifest.abac9ffbc070129adfb2f1cd65f93d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48e4f7f879855f12cc306dc83e221e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9de89cd41163862a7e82",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f4e6b4d93f9948a99d03",
+    "url": "/static/css/main.59d5fe24.chunk.css"
+  },
+  {
+    "revision": "9de89cd41163862a7e82",
+    "url": "/static/js/2.110b9442.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.110b9442.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4e6b4d93f9948a99d03",
+    "url": "/static/js/main.3a4ef7f7.chunk.js"
+  },
+  {
+    "revision": "31a43fa4d6d70a04aaf5",
+    "url": "/static/js/runtime-main.6b319a80.js"
+  }
+]);
